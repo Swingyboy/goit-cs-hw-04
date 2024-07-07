a4f215aaeb05843fc5b4b84a3b92dcc32040a260
serhii <sbezuhlyi@tektelic.com>
2024-07-07 14:47:30 +0300

    Add color output

--- main.py
+++ main.py
@@ -5,26 +5,38 @@ from threading_search import ThreadSearcher
 from process_search import ProcessSearcher
 
 
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+
+
 def main():
     parser = argparse.ArgumentParser(description="Search some files")
     parser.add_argument("-f", "--file-path", required=True)
     parser.add_argument("-p", "--patterns", nargs="*", required=True)
-    parser.add_argument("-n", "--num-threads", type=int, default=5)
+    parser.add_argument("-w", "--workers", type=int, default=5)
     args = parser.parse_args()
 
     files = get_files(args.file_path)
 
     for Searcher in [ThreadSearcher, ProcessSearcher]:
-        searcher = Searcher(files, args.patterns, {}, args.num_threads)
-        time_taken = timeit.timeit(lambda: searcher.run(), number=5)
+        try:
+            searcher = Searcher(files, args.patterns, {}, args.workers)
+            time_taken = timeit.timeit(lambda: searcher.run(), number=5)
+        except Exception as e:
+            print(RED + f"Error occurred while running {Searcher.__name__}: {e}" + RESET)
+            continue
         results = searcher.res
-
-        print(f"Time taken for {Searcher.__name__}: {time_taken}")
-        print(f"Results for {Searcher.__name__}:")
+        print("-" * 50)
+        print(GREEN + f"Time taken for {Searcher.__name__}: {time_taken}" + RESET)
+        print("-" * 50)
+        print(GREEN + f"Results for {Searcher.__name__}:" + RESET)
         for pattern, res in results.items():
-            print(f"Pattern: {pattern}")
+            print(YELLOW + f"Pattern: {pattern}" + RESET)
             for file in res:
                 print(f"\t{file}")
+        print("-" * 50)
 
 
 if __name__ == "__main__":
